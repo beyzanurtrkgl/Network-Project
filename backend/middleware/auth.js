175e3cb63855cf6e9de48872e0cfeaa22e94f62e
beyzanurtrkgl <beyzanurturkoglu06@gmail.com>
2024-05-24 14:17:11 +0300

    Api ayarlandı ve kuallıcı girişi oluşturuldu

---
+++ backend/middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const config = process.env;
+
+const verifyToken= (req,res,next) => {
+    console.log("kontrol ediliyor")
+    const token = req.body.token || req.query.token || req.headers["x-accsess-token"];
+    
+    //token varlığını kontrol etme
+    if(!token){
+        return res.status(403).send("A token is required for authentication");
+    }
+
+    try{
+        const decoded = jwt.verify(token, config.JWT_KEY);
+        req.user = decoded;
+        console.log("kontrol edildi")
+
+
+
+    }catch(err){
+        return res.status(401).send("Invalid Token");
+    }
+    
+    return next(); 
+}
+
+module.exports = verifyToken; 
